e125f6256fa52350147263a9176d76dcc1a73c52
vasquesgabriel91 <vasquesgabriel91@gmail.com>
2025-10-03 20:00:29 -0300

    fix(UserRepository): Create methods for SecretaryRepository.js

---
+++ src/secretary/SecretaryRepository.js
@@ -0,0 +1,16 @@
+import SecretaryModel from "./SecretaryModel.js";
+
+class SecretaryRepository {
+  async create(userData) {
+    const secretary = await SecretaryModel.create(userData);
+    return secretary;
+  }
+
+  async delete(id) {
+    const secretary = await SecretaryModel.findByPk(id);
+    if (!secretary) throw new Error("Usuário não encontrado");
+    await secretary.destroy();
+    return secretary;
+  }
+}
+export default new SecretaryRepository();
